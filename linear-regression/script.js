f8da20622a9b1f8f9c6f1d48af719bd226c5170a
niuyi1017 <1096196462@qq.com>
2020-02-07 13:10:20 +0800

    linear regression

---
+++ linear-regression/script.js
@@ -0,0 +1,31 @@
+import * as tf from "@tensorflow/tfjs"
+import * as tfvis from "@tensorflow/tfjs-vis"
+
+window.onload = async () => {
+  const xs = [1, 2, 3, 4]
+  const ys = [1, 3, 5, 7]
+
+  tfvis.render.scatterplot(
+    { name: "线性回归训练集" },
+    { values: xs.map((x, i) => ({ x, y: ys[i] })) },
+    { xAxisDomain: [0, 5], yAxisDomain: [0, 9] }
+  )
+
+  const model = tf.sequential()
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+  model.compile({ loss: tf.losses.meanSquaredError, optimizer: tf.train.sgd(0.1) })
+
+  const inputs = tf.tensor(xs)
+  const labels = tf.tensor(ys)
+  await model.fit(inputs, labels, {
+    batchSize: 4,
+    epochs: 80,
+    callbacks: tfvis.show.fitCallbacks(
+      { name: "训练过程" },
+      ['loss']
+    )
+  })
+  const outputs = model.predict(tf.tensor([5]))
+  alert(`如果 x 为 5 ，那么预测结果为 ${outputs.dataSync()[0]}`)
+
+}
