07f365007efa03b592118c059bfd84a897427c3c
niuxinyu <1096196462@qq.com>
2020-02-11 13:02:26 +0800

    finished xor

--- xor/script.js
+++ xor/script.js
@@ -1,7 +1,7 @@
 import * as tf from "@tensorflow/tfjs"
 import * as tfvis from "@tensorflow/tfjs-vis"
 import { getData } from "./data.js"
-window.onload = () => {
+window.onload = async () => {
   const data = getData(400)
   tfvis.render.scatterplot(
     { name: "xor 训练" },
@@ -9,6 +9,37 @@ window.onload = () => {
       values: [
         data.filter(p => p.label === 1),
         data.filter(p => p.label === 0)
-    ]}
+      ]
+    }
   )
+
+  const model = tf.sequential()
+  model.add(tf.layers.dense({
+    units: 4,
+    inputShape: [2],
+    activation: 'relu'
+  }))
+  model.add(tf.layers.dense({
+    units: 1,
+    activation: 'sigmoid'
+  }))
+  model.compile({
+    loss: tf.losses.logLoss,
+    optimizer: tf.train.adam(0.1)
+  })
+
+  const inputs = tf.tensor(data.map(p => [p.x, p.y]))
+  const labels = tf.tensor(data.map(p => p.label))
+
+  await model.fit(inputs, labels, {
+    epochs: 50,
+    callbacks: tfvis.show.fitCallbacks(
+      { name: "训练效果" },
+      ['loss']
+    )
+  })
+  window.predict = async (form) => {
+    const pred = await model.predict(tf.tensor([[form.x.value * 1, form.y.value * 1]]))
+    alert(`预测结果为： ${pred.dataSync()[0]}`)
+  }
 }
