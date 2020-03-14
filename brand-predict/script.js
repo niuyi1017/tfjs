d46d0269395c2f1d6fb610bee9489dc779c531f0
niuyi1017 <1096196462@qq.com>
2020-03-15 00:38:18 +0800

    fix bug brand-predict  because of cdn cache

--- brand-predict/script.js
+++ brand-predict/script.js
@@ -1,22 +1,20 @@
 import * as tf  from '@tensorflow/tfjs'
 import { img2x, file2img } from './utils.js'
 
-// const MOBILENET_MODEL_PATH = 'http://127.0.0.1:8080/mobilenet/web_model/model.json'
-// /brand/web_model/model.json
-const MOBILENET_MODEL_PATH = 'http://imooc.niuy.xyz/tfjs/data/mobilenet/web_model/model.json'
-const NUM_CLASSES = 3
+const MODEL_PATH = 'http://imooc.niuy.xyz/tfjs/data'
 const BRAND_CLASSES = ['android', 'apple','windows']
 
 window.onload = async () => {
   
-  const mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH)
+  const mobilenet = await tf.loadLayersModel(MODEL_PATH + '/mobilenet/web_model/model.json')
   mobilenet.summary()
   const layer = mobilenet.getLayer('conv_pw_13_relu')
   const truncateMobilenet = tf.model({
     inputs: mobilenet.inputs,
     outputs: layer.output
   })
-  const model = await tf.loadLayersModel(MOBILENET_MODEL_PATH.replace('mobilenet','brand'))
+  
+  const model = await tf.loadLayersModel(MODEL_PATH + '/brand/web_model/model.json')
   window.predict = async (file) => {
     const img = await file2img(file)
     document.body.appendChild(img)
