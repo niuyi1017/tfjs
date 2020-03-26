e5d7be2c16119695ba5d15037e267c4ae6a49944
niuxinyu <1096196462@qq.com>
2020-03-26 23:53:17 +0800

    train and save speech-zh

--- speech-zh/script.js
+++ speech-zh/script.js
@@ -1,4 +1,5 @@
 import * as speechCommends from "@tensorflow-models/speech-commands"
+import * as tfvis from "@tensorflow/tfjs-vis"
 
 const MODEL_PATH = 'http://imooc.niuy.xyz/tfjs/data'
 let transferRecognizer
@@ -20,4 +21,40 @@ window.collect = async (btn) => {
   )
   btn.disabled = false
   document.querySelector('#count').innerHTML = JSON.stringify(transferRecognizer.countExamples(), null, 2)
+}
+
+window.train = async () => {
+  await transferRecognizer.train({
+    epochs: 30,
+    callback: tfvis.show.fitCallbacks(
+      { name: "训练过程"},
+      ['loss', 'acc'],
+      {callbacks: ['onEpochEnd']}
+    )
+  })
+}
+window.toggle = async (checked) => {
+  if (checked) {
+    console.log(checked)
+    await transferRecognizer.listen(result => {
+      const { scores } = result
+      const labels = transferRecognizer.wordLabels()
+      const index = scores.indexOf(Math.max(...scores))
+      console.log(labels[index])
+    }, {
+        overlapFactor: 0,
+      probabilityThreshold: 0.5,
+    })
+  } else {
+    transferRecognizer.stopListening()
+  }
+}
+
+window.save = () => {
+  const arrayBuffer = transferRecognizer.serializeExamples()
+  const blob = new Blob([arrayBuffer])
+  const link = document.createElement('a')
+  link.href = window.URL.createObjectURL(blob)
+  link.download = "data.bin"
+  link.click()
 }
